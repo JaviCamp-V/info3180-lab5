c6f30833dd1c0c9e23baf96a52aae21b77d5dbf4
JaviCamp-V <54679525+JaviCamp-V@users.noreply.github.com>
2021-03-05 13:35:41 -0500

    Step 5 commit

--- app/__init__.py
+++ app/__init__.py
@@ -4,13 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from .config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-db = SQLAlchemy(app)
 
 # Flask-Login login manager
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+db = SQLAlchemy(app)
 
-app.config.from_object(Config)
 from app import views
